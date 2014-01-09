7c9d795223a34baea47486a0477646b8330551d5
Carlo Baldassi <carlobaldassi@gmail.com>
2014-01-09 13:09:55 +0100

    Revert "remove use of callback API in REPL"
    This reverts commit 9cb1b5cd8b170650e83f3487435da12e94a3ab7a.
    
    Conflicts:
    	base/client.jl

--- base/client.jl
+++ base/client.jl
@@ -35,6 +35,13 @@ exit(n) = ccall(:jl_exit, Void, (Int32,), n)
 exit() = exit(0)
 quit() = exit()
 
+function repl_callback(ast::ANY, show_value)
+    global _repl_enough_stdin = true
+    stop_reading(STDIN) 
+    STDIN.readcb = false
+    put(repl_channel, (ast, show_value))
+end
+
 function repl_cmd(cmd)
     shell = shell_split(get(ENV,"JULIA_SHELL",get(ENV,"SHELL","/bin/sh")))
     # Note that we can't support the fish shell due to its lack of subshells
@@ -135,9 +142,28 @@ function eval_user_input(ast::ANY, show_value)
     isa(STDIN,TTY) && println()
 end
 
-function repl_callback(ast::ANY, show_value)
-    global _repl_enough_stdin = true
-    put(repl_channel, (ast, show_value))
+function read_buffer(stream::AsyncStream, nread)
+    global _repl_enough_stdin::Bool
+    while !_repl_enough_stdin && nb_available(stream.buffer) > 0
+        nread = int(search(stream.buffer,'\n')) # never more than one line or readline explodes :O
+        nread2 = int(search(stream.buffer,'\r'))
+        if nread == 0
+            if nread2 == 0
+                nread = nb_available(stream.buffer)
+            else
+                nread = nread2
+            end
+        else
+            if nread2 != 0 && nread2 < nread
+                nread = nread2
+            end
+        end
+        ptr = pointer(stream.buffer.data,stream.buffer.ptr)
+        skip(stream.buffer,nread)
+        #println(STDERR,stream.buffer.data[stream.buffer.ptr-nread:stream.buffer.ptr-1])
+        ccall(:jl_read_buffer,Void,(Ptr{Void},Cssize_t),ptr,nread)
+    end
+    return false
 end
 
 _eval_done = Condition()
@@ -149,17 +175,7 @@ function run_repl()
 
     # install Ctrl-C interrupt handler (InterruptException)
     ccall(:jl_install_sigint_handler, Void, ())
-    buf = Uint8[0]
-    @async begin
-        while !eof(STDIN)
-            read(STDIN, buf)
-            ccall(:jl_read_buffer,Void,(Ptr{Void},Cssize_t),buf,1)
-            if _repl_enough_stdin
-                wait(_eval_done)
-            end
-        end
-        put(repl_channel,(nothing,-1))
-    end
+    STDIN.closecb = (x...)->put(repl_channel,(nothing,-1))
 
     while true
         if have_color
@@ -169,13 +185,13 @@ function run_repl()
         end
         ccall(:repl_callback_enable, Void, (Ptr{Uint8},), prompt_string)
         global _repl_enough_stdin = false
+        start_reading(STDIN, read_buffer)
         (ast, show_value) = take(repl_channel)
         if show_value == -1
             # exit flag
             break
         end
         eval_user_input(ast, show_value!=0)
-        notify(_eval_done)
     end
 
     if have_color
